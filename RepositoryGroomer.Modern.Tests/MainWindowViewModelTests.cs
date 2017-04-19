b199453b454b1e9d24add6e0804392dc19ffa3db
Pawel Szczygielski <Pawel.Szczygielski@powel.com>
2017-04-19 08:06:19 +0200

    Test project for ModernUI added.

---
+++ RepositoryGroomer.Modern.Tests/MainWindowViewModelTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace RepositoryGroomer.Modern.Tests
+{
+    [TestFixture]
+    public class MainWindowViewModelTests
+    {
+        [Test]
+        public void On_Startup_InitialDirectory_Exists_Projects_Are_Obtained()
+        {
+        }
+
+        [Test]
+        public void On_Startup_InitialDirectory_Not_Exist_Projects_Are_Obtained()
+        {
+        }
+    }
+}
