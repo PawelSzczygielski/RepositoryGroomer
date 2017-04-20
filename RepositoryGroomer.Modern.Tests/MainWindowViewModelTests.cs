04167e6c65cc1635805c826f34ede5c8e13d1d64
Pawel Szczygielski <Pawel.Szczygielski@powel.com>
2017-04-20 13:00:44 +0200

    Issue with starting ModernUI resolved. Bootstrapper extended appropriately with respect to the two new VM ctor's parameters.

--- RepositoryGroomer.Modern.Tests/MainWindowViewModelTests.cs
+++ RepositoryGroomer.Modern.Tests/MainWindowViewModelTests.cs
@@ -8,46 +8,68 @@ namespace RepositoryGroomer.Modern.Tests
     [TestFixture]
     public class MainWindowViewModelTests
     {
-        private readonly IAmConfigurationProvider _configurationProvider;
-        private readonly IAmProjectFileFinder _projectFileFinder;
+        private readonly Mock<IAmConfigurationProvider> _configurationProvider;
+        private readonly Mock<IAmProjectFileFinder> _projectFileFinder;
 
         public MainWindowViewModelTests()
         {
             var configProviderMock = new Mock<IAmConfigurationProvider>();
             configProviderMock.Setup(x => x.SearchPath).Returns(() => "C:\\Repository");
-            _configurationProvider = configProviderMock.Object;
+            _configurationProvider = configProviderMock;
 
             var projectFileFinderMock = new Mock<IAmProjectFileFinder>();
-            projectFileFinderMock.Setup(x => x.GetAllProjects(It.IsAny<string>()))
+            projectFileFinderMock.Setup(x => x.GetAllProjects("C:\\Repository"))
                 .Returns(() => new List<ProjectFileInfo>
                 {
-                    new ProjectFileInfo("ProjFilePath", "DirPath", "ProjectName", new List<LinkedFileInfo>
+                    new ProjectFileInfo("ProjFilePath1", "DirPath1", "ProjectName1", new List<LinkedFileInfo>(),
+                    true),
+                    new ProjectFileInfo("ProjFilePath2", "DirPath2", "ProjectName2", new List<LinkedFileInfo>
                     {
-                        new LinkedFileInfo("RelativePath", LinkTagTypes.Compile, "UnwrappedPath", true)
-                    }, true)
+                        new LinkedFileInfo("RelativePath2", LinkTagTypes.Compile, "UnwrappedPath2", true)
+                    }, true),
+
                 });
-            _projectFileFinder = projectFileFinderMock.Object;
+            projectFileFinderMock.Setup(x => x.GetAllProjects("C:\\OtherRepository")).Returns(() => new List<ProjectFileInfo>
+            {
+                new ProjectFileInfo("ProjFilePath3", "DirPath3", "ProjectName3", new List<LinkedFileInfo>(),
+                    true),
+            });
+
+            _projectFileFinder = projectFileFinderMock;
         }
 
         [Test]
         public void On_Startup_SearchPathProvided_IsDisplayed()
         {
-            var viewModel = new MainWindowViewModel(_configurationProvider, _projectFileFinder);
+            var viewModel = new MainWindowViewModel(_configurationProvider.Object, _projectFileFinder.Object);
             Assert.That(viewModel.SearchPath, Is.EqualTo("C:\\Repository"));
         }
 
         [Test]
         public void On_Startup_ProjectsAreLoaded()
         {
-            var viewModel = new MainWindowViewModel(_configurationProvider, _projectFileFinder);
-            Assert.That(viewModel.TotalNumberOfProjects, Is.EqualTo(1));
+            var viewModel = new MainWindowViewModel(_configurationProvider.Object, _projectFileFinder.Object);
+            Assert.That(viewModel.TotalNumberOfProjects, Is.EqualTo(2));
             Assert.That(viewModel.TotalNumberOfProjectsWithLinkedFiles, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void If_ChangeRepoPathButton_Clicked_And_SearchPath_Not_Changed_Projects_Reloaded()
+        {
+            var viewModel = new MainWindowViewModel(_configurationProvider.Object, _projectFileFinder.Object);
+            viewModel.SearchPathChanged();
+
+            Assert.That(viewModel.TotalNumberOfProjects, Is.EqualTo(2));
         }
 
         [Test]
         public void If_ChangeRepoPathButton_Clicked_New_SearchPath_Obtained_And_Projects_Reloaded()
         {
-            
+            var viewModel = new MainWindowViewModel(_configurationProvider.Object, _projectFileFinder.Object);
+            viewModel.SearchPath = "C:\\OtherRepository";
+            viewModel.SearchPathChanged();
+
+            Assert.That(viewModel.TotalNumberOfProjects, Is.EqualTo(1));
         }
     }
 }
--- RepositoryGroomer.Modern/Bootstrapper.cs
+++ RepositoryGroomer.Modern/Bootstrapper.cs
@@ -1,12 +1,42 @@
-﻿using Caliburn.Micro;
+﻿using System;
+using System.Collections.Generic;
+using Caliburn.Micro;
 using System.Windows;
+using RepositoryGroomer.Core;
+
 namespace RepositoryGroomer.Modern
 {
     public class Bootstrapper : BootstrapperBase
     {
+        readonly SimpleContainer _container = new SimpleContainer();
+
         public Bootstrapper()
         {
             Initialize();
+        }
+        
+        protected override void Configure()
+        {
+            _container.Singleton<IWindowManager, WindowManager>();
+            _container.Singleton<IEventAggregator, EventAggregator>();
+            _container.Singleton<IAmConfigurationProvider, ConfigurationProvider>();
+            _container.Singleton<IAmProjectFileFinder, ProjectFileFinder>();
+            _container.Singleton<MainWindowViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
         }
 
         protected override void OnStartup(object sender, StartupEventArgs e)
