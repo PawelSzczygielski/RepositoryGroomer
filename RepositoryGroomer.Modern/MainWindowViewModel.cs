0898fc71c71fe4969e8cec87326431b00f21e006
Pawel Szczygielski <Pawel.Szczygielski@powel.com>
2017-04-22 13:16:49 +0200

    Removed unused references

--- RepositoryGroomer.Modern/MainWindowViewModel.cs
+++ RepositoryGroomer.Modern/MainWindowViewModel.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Linq;
-using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
 using RepositoryGroomer.Core;
 using Caliburn.Micro;
